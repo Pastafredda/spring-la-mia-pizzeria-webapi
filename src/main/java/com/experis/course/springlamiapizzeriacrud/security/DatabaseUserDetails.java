e557549c408c0ee8e09a9e6988c9c132eea616e9
Pastafredda <justspotify09@gmail.com>
2023-11-22 16:06:15 +0100

    pulsante di logout

--- src/main/java/com/experis/course/springlamiapizzeriacrud/security/DatabaseUserDetails.java
+++ src/main/java/com/experis/course/springlamiapizzeriacrud/security/DatabaseUserDetails.java
@@ -67,4 +67,8 @@ public class DatabaseUserDetails implements UserDetails {
     public boolean isEnabled() {
         return true;
     }
+
+    public Integer getId() {
+        return id;
+    }
 }
