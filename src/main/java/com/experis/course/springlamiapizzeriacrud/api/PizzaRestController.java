b4c53c835841f638520279a67cb2dd9031a3bd3b
Pastafredda <justspotify09@gmail.com>
2023-11-22 18:56:25 +0100

    dettagli

--- src/main/java/com/experis/course/springlamiapizzeriacrud/api/PizzaRestController.java
+++ src/main/java/com/experis/course/springlamiapizzeriacrud/api/PizzaRestController.java
@@ -1,9 +1,12 @@
 package com.experis.course.springlamiapizzeriacrud.api;
 
+import com.experis.course.springlamiapizzeriacrud.exception.PizzaNotFoundException;
 import com.experis.course.springlamiapizzeriacrud.model.Pizza;
 import com.experis.course.springlamiapizzeriacrud.service.PizzaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,5 +24,15 @@ public class PizzaRestController {
     public List<Pizza> index(@RequestParam Optional<String> searchString) {
         //getPizzaList senza il parametro stringa
         return pizzaService.getPizzaList(searchString);
+    }
+
+    //dettagli della pizza per id
+    @GetMapping("/{id}")
+    public Pizza details(@PathVariable Integer id) {
+        try {
+            return pizzaService.getPizzaId(id);
+        } catch (PizzaNotFoundException e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "id della pizza " + id + " non è stato trovato");
+        }
     }
 }
